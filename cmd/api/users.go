7292b0c3e7df74a02792aad46a71fc69cd804069
JoePereiraa <ujoepereira@gmail.com>
2026-05-15 12:04:16 -0300

    docs: add users docs

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -6,6 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetUsers returns all users
+// @Summary		Returns all users
+// @Description	Returns all users
+// @Tags			users
+// @Accept			json
+// @Produce		json
+// @Success		200		{object}	[]database.User
+// @Router			/api/v1/users [get]
+// @Security		BearerAuth
 func (app *application) getAllUsers(c *gin.Context) {
 	users, err := app.models.Users.GetAll()
 
