c17b23982d7b9a3183a802dce6e6c4cc5a6346a3
JoePereiraa <ujoepereira@gmail.com>
2026-05-15 12:01:44 -0300

    feat: add GetAll database.user

--- internal/database/users.go
+++ internal/database/users.go
@@ -51,3 +51,36 @@ func (m *UserModel) GetByEmail(email string) (*User, error) {
 	query := "SELECT * FROM users where email = $1"
 	return m.getUser(query, email)
 }
+
+func (m *UserModel) GetAll() ([]*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "SELECT * FROM users"
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []*User{}
+
+	for rows.Next() {
+		var user User
+
+		err := rows.Scan(&user.Id, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
